6e133dad1384bdf63eafab1dcd3f298b1cf6b192
medaparedd <medapareddysudhasri@gmail.com>
2024-01-27 17:27:00 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -3,9 +3,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+timestamp=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$timestamp.logs"
 n1=$1
 n2=$2
-sum=$(($n1+$n2))
+sum=$(($n1+$n2)) &>> $LOGFILE
 echo -e "$Y $sum $N"
 echo -e "$Y $0 $N"
 echo -e "$Y $@ $N"
