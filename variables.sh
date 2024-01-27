33963e7491f59c3a80d31b53697941d40b340941
medaparedd <medapareddysudhasri@gmail.com>
2024-01-27 17:20:57 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 n1=$1
 n2=$2
 sum=$(($n1+$n2))
-echo "$sum"
-echo "$0"
-echo "$@"
-echo "$#"
+echo "$Y $sum $N"
+echo "$Y $0 $N"
+echo "$Y $@ $N"
+echo "$Y $#"
 if [ $n1 -gt $n2 ]
 then
-    echo "number1 is greater"
+    echo -e "$G number1 is greater $N"
 else
-    echo "number1 is less"
+    echo -e "$R number1 is less $N"
 fi
