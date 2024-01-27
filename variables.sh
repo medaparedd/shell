5e0a75cac03a4f3e0b182d10e7273f0e7a619771
medaparedd <medapareddysudhasri@gmail.com>
2024-01-27 17:23:49 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -6,10 +6,10 @@ N="\e[0m"
 n1=$1
 n2=$2
 sum=$(($n1+$n2))
-echo "$Y $sum $N"
-echo "$Y $0 $N"
-echo "$Y $@ $N"
-echo "$Y.... $# $N"
+echo -e "$Y $sum $N"
+echo -e "$Y $0 $N"
+echo -e "$Y $@ $N"
+echo -e "$Y $#"
 if [ $n1 -gt $n2 ]
 then
     echo -e "$G number1 is greater $N"
