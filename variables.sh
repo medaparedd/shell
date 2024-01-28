bfc4cd492b883c4acbbbbe2cde57dd36e2477496
medaparedd <medapareddysudhasri@gmail.com>
2024-01-28 10:58:23 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -4,11 +4,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-timestamp=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$timestamp.log"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo -e "$Y $0 $N"
 echo -e "$Y $#"
-echo -e "$timestamp"
+echo -e "$TIMESTAMP"
 if [ $ID -ne 0 ]
 then
    echo -e "$R please run with root user"
